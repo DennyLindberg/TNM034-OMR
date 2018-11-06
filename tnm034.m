1361a47a1458f9dac82aad25b9e95f111bc8728d
ElmQ <elias.elmq@gmail.com>
2018-11-06 13:37:54 +0100

    The beginning
    Spooky skeleton code.

---
+++ tnm034.m
@@ -0,0 +1,20 @@
+function strout = tnm034(im)
+%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Im: Inputimage of captured sheet music. Im should be in
+% double format, normalized to the interval [0,1]
+%
+% strout: The resulting character string of the detected
+% notes. The string must follow a pre-defined format.
+%
+% Your program code.
+%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+
+
+
+
+
+
+
+end
