d87731c226502e8e0ab844539c3aa51f5da05b1d
Denny Lindberg <DennyLindberg@users.noreply.github.com>
2018-11-15 11:04:48 +0100

    Improved algorithm for separating notes from the background.

--- prepareforsegmentation.m
+++ prepareforsegmentation.m
@@ -1,8 +1,12 @@
-% Makes notes white (background black) and improves the contrast.
-% Bottom hat filter does some cleanup.
-% Good defaults are botHadDisc = 64, contrast = 0.5
-function [result] = prepareforsegmentation(image, botHatDisc, contrast)
-    result = imbothat(image,strel('disk', botHatDisc));  % Apply bottom hat filter
-    result = imadjust(result, [0.0, contrast]);          % Increase contrast
+% Attempts to remove background and isolates the notes
+function [result] = prepareforsegmentation(image)
+    % Version 1 - params (image, botHatDisc = 64, contrast = 0.5)
+    %result = imbothat(image,strel('disk', botHatDisc));  % Apply bottom hat filter
+    %result = imadjust(result, [0.0, contrast]);          % Increase contrast
+    
+    % Version 2
+    background = imclose(image, strel('Disk',10));
+    extractedNotes = imsubtract(background, image);
+    result = imadjust(extractedNotes, [0.1, 0.5]);  % these values are arbitrary but seems to work well in most cases
 end
 
