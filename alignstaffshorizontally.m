1c68f0edd379ca98a1f4e74ef4f8479cf8be9c1f
Denny Lindberg <DennyLindberg@users.noreply.github.com>
2018-11-16 12:01:26 +0100

    Improved line detection for correcting rotated scan.

--- alignstaffshorizontally.m
+++ alignstaffshorizontally.m
@@ -1,5 +1,10 @@
 % This corrects a slightly rotated image. (won't correct perspective
 % distortion)
+
+% TODO: 
+%   houghpeaks currently have 10 max lines, can this be set automatic?
+%   houghlines has FillGap and MinLength as arbitrary values. Automate how?
+
 function [alignedResult] = alignstaffshorizontally(image, horizontalAngleThreshold, bitmapThreshold)
     % Default parameter values
     levelThreshold = 0.8;
@@ -10,14 +15,11 @@ function [alignedResult] = alignstaffshorizontally(image, horizontalAngleThresho
         horizontalAngleThreshold = horizontalAngleThreshold;
     end
     
-    % Use thresholded image for detecing lines
-    imageThreshold = image > levelThreshold;
-    
     % Extract lines
-    imageEdges = edge(imageThreshold, 'canny');
-    [H, T, R] = hough(imageEdges, 'Theta', -90:0.1:89.9); % T = Theta, R = Rho
-    P = houghpeaks(H,2);
-    lines = houghlines(imageEdges, T, R, P);
+    imageBW = image < graythresh(image);
+    [H, T, R] = hough(imageBW, 'Theta', -90:0.1:89.9); % T = Theta, R = Rho
+    P = houghpeaks(H, 10);
+    lines = houghlines(imageBW, T, R, P, 'FillGap', 200, 'MinLength', 50);
     
     % Calculate the average angle of the lines
     Tavg = 0;
