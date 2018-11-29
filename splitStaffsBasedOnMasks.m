cd4d2f50ea6c43d4aad1e949cdc67dcff0325327
Denny Lindberg <DennyLindberg@users.noreply.github.com>
2018-11-29 18:21:31 +0100

    Code now removes clutter outside the staffs and notes. The split of staffs have been improved. tnm034 was reverted to a previous commit.

---
+++ splitStaffsBasedOnMasks.m
@@ -0,0 +1,104 @@
+function [individualStaffs, staffCount] = splitStaffsBasedOnMasks(image, staffsMask, notesMask)  
+    % Default return values
+    individualStaffs = [];
+    staffCount = 0;
+    
+    % Detect that input dimensions agree
+    imageWidth = size(image, 2);
+    imageHeight = size(image, 1);
+    maskWidth = size(staffsMask, 2);
+    maskHeight = size(staffsMask, 1);
+    if imageWidth == 0 || imageHeight == 0 || maskWidth == 0 || maskHeight == 0 || ...
+       imageWidth ~= maskWidth || imageHeight ~= maskHeight
+        return;
+    end
+    
+    % Extract properties (exit early if no properties found)
+    notesProps = regionprops(notesMask, 'BoundingBox');
+    staffCount = size(notesProps, 1);
+    if staffCount == 0; return; end
+    
+    % Sort props based on bounding box vertical order
+    if staffCount > 0
+        bboxes = cat(1, notesProps.BoundingBox);
+        [sorted, sort_order] = sort(bboxes(:, 2));
+        notesProps = notesProps(sort_order);
+    end
+    
+    % Generate list of boxes for easier processing
+    limits = [];
+    for i=1:staffCount   
+        bbox = notesProps(i).BoundingBox;
+        x1 = floor(bbox(1));
+        y1 = floor(bbox(2));
+        x2 = x1 + floor(bbox(3));
+        y2 = y1 + floor(bbox(4));
+        
+        % Remove bad staffs
+        cx = round((x1+x2)/2);
+        cy = round((y1+y2)/2);
+        limitX = imageWidth*0.02;
+        limitY = imageHeight*0.02;
+        if cx < limitX || cx > (imageWidth-limitX) || ...
+           cy < limitY || cy > (imageHeight-limitY)
+           %disp("Removed bad staff, the midpoint was too close to the image border");
+           continue;
+        end
+        
+        % Extract current staff mask and calculate the bbox of it
+        staffY1 = y1;
+        staffY2 = y2;
+        localMask = zeros(size(notesMask));
+        localMask(y1:y2, x1:x2) = 1;
+        localMask = and(localMask, staffsMask);
+        staffProps = regionprops(localMask, 'BoundingBox');
+        if ~isempty(staffProps)
+            firstProperty = staffProps(1);
+            staffY1 = floor(firstProperty.BoundingBox(2));
+            staffY2 = staffY1 + floor(firstProperty.BoundingBox(4));
+        end
+        
+        newLimit = struct;
+        newLimit.x1 = floor(bbox(1));
+        newLimit.y1 = floor(bbox(2));
+        newLimit.x2 = x1 + floor(bbox(3));
+        newLimit.y2 = y1 + floor(bbox(4));
+        newLimit.staffY1 = staffY1;
+        newLimit.staffY2 = staffY2;
+        
+        limits = [limits; newLimit];
+    end
+    staffCount = size(limits,1);
+    
+    % Extract staff segments
+    individualStaffs = [];
+    for i=1:staffCount
+        yBegin = limits(i).y1;
+        yEnd   = limits(i).y2;
+        xBegin = limits(i).x1;
+        xEnd   = limits(i).x2;
+        
+        if i==staffCount && i > 1
+            % Don't crop the last staff if it is much shorter
+            % than the previous staff.
+            scale = (xEnd-xBegin)/(limits(i-1).x2-limits(i-1).x1);
+            if scale < 0.95
+                xEnd = xBegin + (limits(i-1).x2-limits(i-1).x1);
+            end
+        end
+
+        % Crop based on the new region
+        staff = struct;
+        staff.image = image(yBegin:yEnd, xBegin:xEnd);
+        staff.staffMask = staffsMask(yBegin:yEnd, xBegin:xEnd); 
+        staff.notesMask = notesMask(yBegin:yEnd, xBegin:xEnd); 
+        
+        % New bounding box width is the width of the image segment.
+        % New height is the same but has the vertical cut as origin.
+        staff.top = max(1, limits(i).staffY1-yBegin);
+        staff.bottom = max(1, limits(i).staffY2-yBegin);
+        
+        individualStaffs = [individualStaffs; staff];
+    end
+end
+
--- tnm034.m
+++ tnm034.m
@@ -9,85 +9,125 @@ function strout = tnm034(im)
 % Your program code.
 %%%%%%%%%%%%%%%%%%%%%%%%%%
 
-imagePath = 'Images/TestStaff.jpg';
+imagePath = 'Images/im10c.jpg';
 drawDebug_alternatingStaffs = true;
+staffNormalizedWidth = 2048;
 
 %% Pre-processing (Grade 4/5)
 
 
 %% Geometric transform (Denny)
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Load image and remove background
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-original = imread(imagePath);
-originalgray = im2double(rgb2gray(original)); 
-[image, region] = removeBackground(originalgray);   
-image = image(region(2):region(4), region(1):region(3));
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % Load image and remove background
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    original = imread(imagePath);
+    originalgray = im2double(rgb2gray(original)); 
+    [notes, region] = removeBackground(originalgray);   
+    notes = notes(region(2):region(4), region(1):region(3));
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Extract alpha of staff lines and create an enclosing
-% mask for each staff.
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-lineSearch_angleLimit = 10;
-lineSearch_angleStep = 1;
-lineSearch_minimumLength = round(size(image,2)*0.4);
+    
+    
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % Extract alpha of staff lines and create an enclosing
+    % mask for each staff.
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    lineSearch_angleLimit = 10;
+    lineSearch_angleStep = 1;
+    lineSearch_minimumLength = round(size(notes,2)*0.4);
+    
+    % Expand image, otherwise the structuring element might fail near the corners
+    paddingWidth = round(lineSearch_minimumLength*0.2);
+    paddingHeight = round(paddingWidth*0.2);
+    notes = padarray(notes, [paddingHeight, paddingWidth], 1, 'both');
+    
+    % Attempt to extract the masks
+    staffLinesAlpha = getLinesAlphaByAngle(notes, lineSearch_angleLimit, lineSearch_angleStep, lineSearch_minimumLength);
+    staffsMask = imclose(staffLinesAlpha, strel('disk', 16, 4));
+    staffsMask = (staffsMask > graythresh(staffsMask));
 
-% Expand image, otherwise the structuring element might fail near the corners
-paddingWidth = round(lineSearch_minimumLength*0.2);
-paddingHeight = round(paddingWidth*0.2);
-image = padarray(image, [paddingHeight, paddingWidth], 1, 'both');
+    
+    
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % Use mask of staffs to detect perspective transform
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    perspective = estimatePerspectiveTransform(staffsMask);
+    perspectiveInverse = invert(perspective);
+    
+    
+    
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % Apply perspective correction to image and alpha
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    notes = 1-imwarp(1-notes, perspectiveInverse, 'cubic');
+    staffLinesAlpha = imwarp(staffLinesAlpha, perspectiveInverse, 'cubic');
+    
+    
+    
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % Recreate staffsmask post-transform. 
+    % Also create a new mask which includes notes hanging
+    % outside the staff.
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    staffsMask = imclose(staffLinesAlpha, strel('disk', 16, 4));
+    staffsMask = (staffsMask > graythresh(staffsMask));
+    
+    notesMask = (notes < graythresh(notes));
+    notesMask = or(notesMask, staffsMask);
+    notesMask = bwareaopen(notesMask, size(notes,2));
+    
+    
+    
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % Use the mask to remove clutter
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    notes = 1-(1-notes).*notesMask;  
 
-% Attempt to extract the masks
-staffLinesAlpha = getLinesAlphaByAngle(image, lineSearch_angleLimit, lineSearch_angleStep, lineSearch_minimumLength);
-staffsMask = imclose(staffLinesAlpha, strel('disk', 16, 4));
-staffsMask = (staffsMask > graythresh(staffsMask));
-
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Use mask of staffs to detect perspective transform
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-perspective = estimatePerspectiveTransform(staffsMask);
-perspectiveInverse = invert(perspective);
-
-
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Apply perspective correction to image and alpha
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-image = 1-imwarp(1-image, perspectiveInverse, 'cubic');
-staffLinesAlpha = imwarp(staffLinesAlpha, perspectiveInverse, 'cubic');
-staffsMask = imclose(staffLinesAlpha, strel('disk', 16, 4));
-staffsMask = (staffsMask > graythresh(staffsMask));
-
-
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Split image into a separate segment for each staff
-% based on the mask.
-%
-% staff (struct)
-%   .image      bitmap containing notes
-%   .mask       logical mask which wraps around the five major staff lines
-%   .top        top of first staff line
-%   .bottom     bottom of fifth staff line
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-[staffs, staffCount] = splitStaffsBasedOnMask(image, staffsMask);
-
-
-if drawDebug_alternatingStaffs
+    
+    
+    
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % Split image into a separate segment for each staff
+    % based on the masks.
+    %
+    % staff (struct)
+    %   .image      bitmap containing notes
+    %   .staffMask  logical mask which wraps around the five major staff lines
+    %   .notesMask  logical mask which also includes the notes hanging outside the staff
+    %   .top        top of first staff line
+    %   .bottom     bottom of fifth staff line
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    [staffs, staffCount] = splitStaffsBasedOnMasks(notes, staffsMask, notesMask);
+    
+    % Normalize
     for k=1:staffCount
-        staffs(k).image = 1-(1-staffs(k).image)*0.2;
-        staffs(k).image(staffs(k).top, :) = 0;
-        staffs(k).image(staffs(k).bottom, :) = 0;
-
-        % invert alternating
-        if ~mod(k,2)
-            staffs(k).image = 1-staffs(k).image;
-        end
+        scaleFactor = staffNormalizedWidth / size(staffs(k).image, 2);
+        
+        staffs(k).image     = imresize(staffs(k).image, scaleFactor, 'bicubic');
+        staffs(k).staffMask = imresize(staffs(k).staffMask, scaleFactor, 'nearest');
+        staffs(k).notesMask = imresize(staffs(k).notesMask, scaleFactor, 'nearest');
+        
+        staffs(k).top = round(staffs(k).top*scaleFactor);
+        staffs(k).bottom = round(staffs(k).bottom*scaleFactor);
     end
-    figure;
-    imshow(vertcat(staffs.image));
-end
+    
+    % DRAW A DEBUG LINE ON TOP/BOTTOM OF EACH STAFF
+    if drawDebug_alternatingStaffs
+        for k=1:staffCount
+            staffs(k).image = 1-(1-staffs(k).image)*0.1;
+            
+            line1 = [max(1, (staffs(k).top-3)):min(size(staffs(k).image,1), (staffs(k).top+3))];
+            line2 = [max(1, (staffs(k).bottom-3)):min(size(staffs(k).image,1), (staffs(k).bottom+3))];
+            staffs(k).image(line1, :) = 0;
+            staffs(k).image(line2, :) = 0;
+            
+            % invert alternating
+            if ~mod(k,2)
+                staffs(k).image = 1-staffs(k).image;
+            end
+        end
+        figure;
+        imshowpair(originalgray, vertcat(staffs.image), 'montage');
+    end
 
 
 
@@ -127,9 +167,9 @@ centroids = centroids(sortIndex,:);
 
 pitches = findPitch(centroids);
 
-4ths = [G1 A1 B1 C2 D2 E2 F2 G2 A2 B2 C3 D3 E3 F3 G3 A3 B3 C4 D4 E4];
+fourths = [G1 A1 B1 C2 D2 E2 F2 G2 A2 B2 C3 D3 E3 F3 G3 A3 B3 C4 D4 E4];
 
-8ths = [g1 a1 b1 c2 d2 e2 f2 g2 a2 b2 c3 d3 e3 f3 g3 a3 b3 c4 d4 e4];
+eights = [g1 a1 b1 c2 d2 e2 f2 g2 a2 b2 c3 d3 e3 f3 g3 a3 b3 c4 d4 e4];
 %%
  L = bwlabel(noteheads,4);
  Lmax = max(max(L));
