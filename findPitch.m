bf1f2556a9b2b36f5307e6391930957cf6ff2b24
thobbe <thobias.joelson@gmail.com>
2018-12-03 11:11:23 +0100

    findpitch changed, elise add your stuff

--- findPitch.m
+++ findPitch.m
@@ -1,19 +1,10 @@
 
-function [Pitch] = findPitch(centroidArray)
-
-Point = [x2;y2];
+function Pitch = findPitch(Point, Origo, y)
 
 BoxSize = Point - Origo; 
 
 Dist = BoxSize(1)/8;
-Pitch = zero(length(centroidArray),1);
 
+Pitch = (y-Origo) / Dist;   
 
-% följande for-loop kanske ska skrivas som en liten separat funktion i
-% rapport syfte som förklaras lite mer utförligt och sedan nämna att vi gör
-% detta för alla ceteroider. 
-for  i = 1 : length(centroidArray) 
-    distfromorigo = centroidArray(i) - Origo(2);
-    Pitch(i) = distfromorigo / Dist;   
-    % origo(2) + error; %nån form av detta kan behövas nånstans i loopen 
-end 
+end
